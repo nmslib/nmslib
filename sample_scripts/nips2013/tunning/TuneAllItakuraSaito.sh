b279d2dcd2d2d642a8a3d93fd6e4e9abeeed559b
searchivarius <leo@boytsov.info>
2013-11-03 02:03:48 -0500

    Tunning results for Itakura-Saito distance.

---
+++ sample_scripts/nips2013/tunning/TuneAllItakuraSaito.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RecallList="0.99 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2"
+BucketSize="50"
+
+for K in 1 ; do
+  ../../TuneOneMeth.sh "$RecallList" ~/DataNIPS/sig10k.txt ItakuraSaitoFast  1111 0.1 0.1 $K ResultsItakuraSaito/OutFile.sig10k.$K $BucketSize
+  if [ "$?" != "0" ] ; then
+    echo "Tunning failed!"
+    exit 1
+  fi
+
+  for n in 8 16 32 64 128 256 ; do 
+    ../../TuneOneMeth.sh "$RecallList" ~/DataNIPS/final$n.txt ItakuraSaitoFast  $n 0.1 0.1 $K ResultsItakuraSaito/OutFile.final$n.$K $BucketSize
+    if [ "$?" != "0" ] ; then
+      echo "Tunning failed!"
+      exit 1
+    fi
+  done
+done
