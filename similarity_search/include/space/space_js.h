706636d511f035eebdc9f77cb7f72e70ae0529af
searchivarius <leo@boytsov.info>
2013-12-11 04:26:43 -0500

    Added the Jensen-Shannon distance.

---
+++ similarity_search/include/space/space_js.h
@@ -0,0 +1,56 @@
+/**
+ * Non-metric Space Library
+ *
+ * Authors: Bilegsaikhan Naidan (https://github.com/bileg), Leonid Boytsov (http://boytsov.info).
+ * With contributions from Lawrence Cayton (http://lcayton.com/).
+ *
+ * For the complete list of contributors and further details see:
+ * https://github.com/searchivarius/NonMetricSpaceLib 
+ * 
+ * Copyright (c) 2010--2013
+ *
+ * This code is released under the
+ * Apache License Version 2.0 http://www.apache.org/licenses/.
+ *
+ */
+
+#ifndef _SPACE_LP_H_
+#define _SPACE_LP_H_
+
+#include <string>
+#include <map>
+#include <stdexcept>
+
+#include <string.h>
+#include "global.h"
+#include "object.h"
+#include "utils.h"
+#include "space.h"
+#include "space_vector.h"
+
+#define SPACE_JS_SLOW         "jsslow"
+#define SPACE_JS_FAST         "jsfast"
+#define SPACE_JS_FAST_APPROX  "jsfastapprox"
+
+namespace similarity {
+
+template <typename dist_t>
+class JSSpace : public VectorSpace<dist_t> {
+ public:
+  enum JSType {kJSSlow, kJSFastPrecomp, kJSFastPrecompApprox};
+
+  explicit JSSpace(JSType type) : type_(type) {}
+  virtual ~JSSpace() {}
+
+  virtual std::string ToString() const;
+
+ protected:
+  virtual dist_t HiddenDistance(const Object* obj1, const Object* obj2) const;
+ private:
+  JSType   type_;
+};
+
+
+}  // namespace similarity
+
+#endif 
---
+++ similarity_search/src/factory/space/space_js.cc
@@ -0,0 +1,63 @@
+/**
+ * Non-metric Space Library
+ *
+ * Authors: Bilegsaikhan Naidan (https://github.com/bileg), Leonid Boytsov (http://boytsov.info).
+ * With contributions from Lawrence Cayton (http://lcayton.com/).
+ *
+ * For the complete list of contributors and further details see:
+ * https://github.com/searchivarius/NonMetricSpaceLib 
+ * 
+ * Copyright (c) 2010--2013
+ *
+ * This code is released under the
+ * Apache License Version 2.0 http://www.apache.org/licenses/.
+ *
+ */
+
+#include "searchoracle.h"
+#include "space_js.h"
+#include "spacefactory.h"
+
+namespace similarity {
+
+/*
+ * Creating functions.
+ */
+
+template <typename dist_t>
+Space<dist_t>* CreateJSSlow() {
+  return new JSSpace<dist_t>(JSSpace<dist_t>::kJSSlow);
+}
+
+template <typename dist_t>
+Space<dist_t>* CreateJSFastPrecomp() {
+  return new JSSpace<dist_t>(JSSpace<dist_t>::kJSFastPrecomp);
+}
+
+template <typename dist_t>
+Space<dist_t>* CreateJSFastPrecompApprox() {
+  return new JSSpace<dist_t>(JSSpace<dist_t>::kJSFastPrecompApprox);
+}
+
+
+/*
+ * End of creating functions.
+ */
+
+/*
+ * Let's register creating functions in a method factory.
+ *
+ * IMPORTANT NOTE: don't include this source-file into a library.
+ * Sometimes C++ carries out a lazy initialization of global objects
+ * that are stored in a library. Then, the registration code doesn't work.
+ */
+
+REGISTER_SPACE_CREATOR(float,  SPACE_JS_SLOW, CreateJSSlow)
+REGISTER_SPACE_CREATOR(double, SPACE_JS_SLOW, CreateJSSlow)
+REGISTER_SPACE_CREATOR(float,  SPACE_JS_FAST, CreateJSFastPrecomp)
+REGISTER_SPACE_CREATOR(double, SPACE_JS_FAST, CreateJSFastPrecomp)
+REGISTER_SPACE_CREATOR(float,  SPACE_JS_FAST_APPROX, CreateJSFastPrecompApprox)
+REGISTER_SPACE_CREATOR(double, SPACE_JS_FAST_APPROX, CreateJSFastPrecompApprox)
+
+}
+
