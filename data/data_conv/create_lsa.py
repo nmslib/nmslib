3ddd971ca26c16af2c00ec1ed2271c3405c6ef79
searchivarius <leo@boytsov.info>
2014-01-31 02:41:37 -0500

    new data conversion scripts

---
+++ data/data_conv/create_lsa.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import logging, gensim, bz2
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# load id->word mapping (the dictionary), one of the results of step 2 above
+id2word = gensim.corpora.Dictionary.load_from_text('sparse_wiki_wordids.txt')
+# load corpus iterator
+mm = gensim.corpora.MmCorpus(bz2.BZ2File('sparse_wiki_tfidf.mm.bz2')) 
+
+print mm
+
+ntop=512
+
+lsi = gensim.models.lsimodel.LsiModel(corpus=mm, id2word=id2word, num_topics=ntop, chunksize=10000)
+
+lsi_file = 'LSI/lsi'+str(ntop)
+
+lsi.save(lsi_file)
+
+out_vect = 'LSI/wikipedia_lsi'+str(ntop)+'.txt'
+gensim.corpora.MmCorpus.serialize(out_vect, (gensim.matutils.unitvec(vec) for vec in lsi[mm]))
+
+
