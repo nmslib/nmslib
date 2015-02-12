b03628dc56b272195b43c948869469b2edbe813d
searchivarius <leo@boytsov.info>
2015-02-11 20:46:04 -0500

    Fixing scripts to create LDA data with gensim.

--- data/data_conv/create_lda_multicore.py
+++ data/data_conv/create_lda_multicore.py
@@ -5,9 +5,11 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 # load id->word mapping (the dictionary), one of the results of step 2 above
 id2word = gensim.corpora.Dictionary.load_from_text('sparse_wiki_wordids.txt')
 # load corpus iterator
-mm = gensim.corpora.MmCorpus(bz2.BZ2File('sparse_wiki_tfidf.mm.bz2')) 
+mmTrain = gensim.corpora.MmCorpus(bz2.BZ2File('sparse_wiki_tfidf_part1.mm.bz2')) 
+mmTest = gensim.corpora.MmCorpus(bz2.BZ2File('sparse_wiki_tfidf_part2.mm.bz2')) 
 
-print mm
+print mmTrain
+print mmTest
 
 if len(sys.argv) != 3:
   raise Exception("Usage: <number of topics> <number of cores>")
@@ -16,12 +18,13 @@ ntop=int(sys.argv[1])
 ncores=int(sys.argv[2])
 print "Using " + str(ntop) + " topics and " + str(ncores) + " cores"
 
-lda = gensim.models.LdaMulticore(corpus=mm, id2word=id2word, num_topics= ntop, workers=ncores)
+lda = gensim.models.LdaMulticore(corpus=mmTrain, id2word=id2word, num_topics= ntop, workers=ncores)
 
 lda_file = 'LDA/lda'+str(ntop)
 
 lda.save(lda_file)
 
 out_vect = 'LDA/wikipedia_lda'+str(ntop)+'.txt'
-gensim.corpora.MmCorpus.serialize(out_vect, (gensim.matutils.unitvec(vec) for vec in lda[mm]))
+#gensim.corpora.MmCorpus.serialize(out_vect, (gensim.matutils.unitvec(vec) for vec in lda[mmTest]))
+gensim.corpora.MmCorpus.serialize(out_vect, lda[mmTest])
 
