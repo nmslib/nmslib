b4050af44ed5deb597377a48e94afb7415e70c0b
searchivarius <leo@boytsov.info>
2016-08-02 17:18:25 -0400

    deleting unnecessary stuff (for this NMSLIB version).

--- data/get_all_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./download_wikipedia_sparse.sh
-./download_wikipedia_lsi128.sh
-./download_cayton.sh
-./download_colors.sh
-./genunif.py -d 64 -n 500000 -o unif64.txt
-
--- data/get_data_nips2013.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./download_cayton.sh
-./download_colors.sh
-./genunif.py -d 64 -n 500000 -o unif64.txt
-
