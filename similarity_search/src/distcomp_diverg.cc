894634e249ceb81f7c78823309240a3ab0fa9c07
searchivarius <leo@boytsov.info>
2016-09-10 16:48:19 -0400

    now one more fix for Renyi divergence

--- similarity_search/src/distcomp_diverg.cc
+++ similarity_search/src/distcomp_diverg.cc
@@ -40,14 +40,16 @@ template  float alpha_beta_divergence(const float* x, const float* y, const int 
 template  double alpha_beta_divergence(const double* x, const double* y, const int length, float alpha, float beta);
 
 template <typename T> T renyi_divergence(const T* x, const T* y, const int length, float alpha) {
-  static T minCheck = 0.9999; // somewhat arbitrarily
-  T res = 0;
-  float beta = 1.0 - alpha; 
+  T sum = 0;
+  T eps = -1e-6;
+  float t = alpha-1; 
   for (int i = 0; i < length; ++i) {
-    res += pow(x[i], alpha)*pow(y[i], beta);  
+    sum += x[i]*pow(x[i]/y[i], t);
   } 
-  CHECK_MSG(res >= minCheck, "Check failed sum in Renyi diverg. is supposed to be >= 1, but it's " + ConvertToString(res) + " for alpha=" + ConvertToString(alpha)); 
-  return log(max<T>(1,res));
+  float res = 1/t * log(sum);
+  CHECK_MSG(res >= eps, "Expected a non-negative result, but got " + ConvertToString(res) + " for alpha="  + ConvertToString(alpha));
+  // Might be slightly negative due to rounding errors
+  return max<T>(0, res);
 }
 
 template  float renyi_divergence(const float* x, const float* y, const int length, float alpha);
