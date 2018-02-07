e80341f0cff4720b58543409d284ffa6efb65b26
searchivairus <leo@boytsov.info>
2018-02-07 00:07:59 -0500

    intermediate commit

--- scripts/update_all_files_header.sh
+++ scripts/update_all_files_header.sh
@@ -5,6 +5,10 @@ for suff in h cc cpp c ; do
   for f in `find . -name *.$suff` ; do
     echo $f
     scripts/update_file_header.py $f $tmpn
+    if [ "$?" = "0" ] ; then
+      cat $tmpn > $f
+    fi
   done
 done
+rm $tmpn
 
