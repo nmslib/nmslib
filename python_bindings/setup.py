3dd384e45366605a1fecc9da1b3a759ad114350b
searchivairus <leo@boytsov.info>
2018-02-18 16:36:58 -0500

    Reducing the size of the line (or else travis complains).

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -127,7 +127,7 @@ setup(
     name='nmslib',
     version=__version__,
     description='Non-Metric Space Library (NMSLIB)',
-    author='Bilegsaikhan Naidan, Leonid Boytsov, Yury Malkov, Ben Frederickson, David Novak, and others',
+    author='B. Naidan, L. Boytsov, Yu. Malkov, B. Frederickson, D. Novak, et al.',
     url='https://github.com/searchivarius/nmslib',
     long_description="""Non-Metric Space Library (NMSLIB) is an efficient cross-platform
  similarity search library and a toolkit for evaluation of similarity search methods. The
