a51d37881bc7e4eea2a5ae4cc99a7c2cbc724ac5
Greg Friedland <greg@atomwise.com>
2019-02-18 19:22:54 -0800

    fix typos

--- python_bindings/tests/jaccard_comparison.sh
+++ python_bindings/tests/jaccard_comparison.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-for space in bit_jaccard jaccard_sparse; do
+set -e
+
+for space in jaccard_sparse; do
   for num_elems in 30000  100000 300000 1000000 3000000 10000000 30000000; do
-    for nbits in 512 20148; do
+    for nbits in 512 2048; do
       python jaccard_comparison.py $space $num_elems $nbits
     done
   done
