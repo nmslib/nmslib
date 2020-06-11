5269d82aba7cb223e75c6ce5d8545af8be7e59a1
Jamie Brunning <jamesb@ilab.local>
2020-06-11 19:10:12 +0100

    Fix version specification - need ==

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -11,7 +11,7 @@ if sys.platform.startswith("win") and struct.calcsize("P") * 8 == 32:
     raise RuntimeError("Windows 32-bit is not supported.")
 
 dep_list = ['pybind11>=2.2.3', 'psutil']
-dep_list.append("numpy>=1.10.0,<1.17 ; python_version='2.7'")
+dep_list.append("numpy>=1.10.0,<1.17 ; python_version=='2.7'")
 dep_list.append("numpy>=1.10.0 ; python_version>='3.5'")
 
 py_version = tuple([int(v) for v in sys.version.split('.')[:2]])
