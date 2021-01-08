a7e81b873fb5bb6a39158659a1328f5c1e41d05a
searchivarius <leo@boytsov.info>
2021-01-08 03:39:09 -0500

    ..

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 import struct
 
-__version__ = '2.0.12'
+__version__ = '2.0.13'
 
 if sys.platform.startswith("win") and struct.calcsize("P") * 8 == 32:
     raise RuntimeError("Windows 32-bit is not supported.")
