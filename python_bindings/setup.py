4412781301c919018e155a45c12c08e28a776e1c
Jaeho Shin <netj@sparcs.org>
2021-06-12 09:11:07 -0700

    clang -march=native breaks on macOS with Apple Silicon

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import sys
@@ -130,6 +131,8 @@ class BuildExt(build_ext):
     }
 
     if sys.platform == 'darwin':
+        if platform.processor() == 'arm64':
+            c_opts['unix'].remove('-march=native')
         c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
         link_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
     else:
