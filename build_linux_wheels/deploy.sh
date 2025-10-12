ced00b1f279945158b1bed7acd0ded59f33fbeb7
searchivarius <leo@boytsov.info>
2025-10-11 23:23:12 -0400

    upgrading the manylinux

--- build_linux_wheels/deploy.sh
+++ build_linux_wheels/deploy.sh
@@ -13,20 +13,17 @@ echo "Building $arch wheels"
 if [ "$arch" == "aarch64" -o "$arch" == "arm64" ]; then
     CFLAGS="-march=armv8-a"
     PRE_CMD=""
-    PLAT=manylinux2014_aarch64
-    DOCKER_IMAGE=quay.io/pypa/$PLAT
-    docker pull $DOCKER_IMAGE
-    docker run --rm -v `pwd`:/io -e PLAT=$PLAT -e PYTHON=$PYTHON -e CFLAGS=$CFLAGS $DOCKER_IMAGE $PRE_CMD /io/build_linux_wheels/build-wheels.sh
+    for PLAT in manylinux2014_aarch64 ; do
+        DOCKER_IMAGE=quay.io/pypa/$PLAT
+        docker pull $DOCKER_IMAGE
+        docker run --rm -v `pwd`:/io -e PLAT=$PLAT -e PYTHON=$PYTHON -e CFLAGS=$CFLAGS $DOCKER_IMAGE $PRE_CMD /io/build_linux_wheels/build-wheels.sh
+    done
 else
     CFLAGS=-msse2
     echo "Ignoring manylinux1_i686"
-    for PLAT in manylinux1_x86_64 manylinux2010_x86_64; do
+    PRE_CMD=""
+    for PLAT in manylinux2014_x86_64; do
         DOCKER_IMAGE=quay.io/pypa/$PLAT
-        if [ "$PLAT" == "manylinux1_i686" ]; then
-            PRE_CMD=linux32
-        else
-            PRE_CMD=""
-        fi
         docker pull $DOCKER_IMAGE
         docker run --rm -v `pwd`:/io -e PLAT=$PLAT -e PYTHON=$PYTHON -e CFLAGS=$CFLAGS $DOCKER_IMAGE $PRE_CMD /io/build_linux_wheels/build-wheels.sh
     done
